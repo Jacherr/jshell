e54eee2130758a80f746beeec28e516fe27199b7
Jacherr <jwc2002@outlook.com>
2025-01-15 12:12:53 +0000

    define more stuf

--- src/parse.h
+++ src/parse.h
@@ -7,38 +7,45 @@
 
 enum lex_entry_type_t
 {
-    LE_COMMAND_NAME,
-    LE_COMMAND_ARG,
-    LE_COMMAND_SEP
+    /* path or program name found in PATH */
+    LEX_COMMAND_NAME,
+    /* program arguments */
+    LEX_COMMAND_ARG,
+    /* >>, >, etc */
+    LEX_COMMAND_REDIRECT,
+    /* ; */
+    LEX_COMMAND_SEP,
+    /* a command embedded in another, i.e. $(cat x) */
+    LEX_COMMAND_SUB
 };
 
-struct lex_data_run_program
+struct lex_data_redirect_fd
 {
-    char* program_name;
-    char** program_argv;
-    int program_argc;
+    int fd;
 };
 
 struct lex_entry_t
 {
     enum lex_entry_type_t type;
-    union {
-        struct lex_data_run_program lex_data_run_program;
-    } data;
+    /* any additional data based on the type */
+    union lex_data {
+        /* LEX_COMMAND_REDIRECT */
+        struct lex_data_redirect_fd redirect_fd;
+        /* where the data is just the string format of the arg(s)
+           for example a program or path name, or argument or set of arguments joined via "" or ''
+           LEX_COMMAND_NAME LEX_COMMAND_ARG 
+        */
+        char* data_string;
+        /* embedded command, i.e. $(cat x)
+           LEX_COMMAND_SUB
+         */
+        struct lex_entry_t* sub_lex;
+        /* where there is no data
+           LEX_COMMAND_SEP
+        */
+        char data_none;
+    } lex_data;
     struct lex_entry_t* next;
-};
-
-enum commandline_action_type_t
-{
-    CLA_RUNCOMMAND,
-    CLA_REDIRECT,
-    CLA_PIPE
-};
-
-struct commandline_action_chain
-{
-    enum commandline_action_type_t type;
-    
 };
 
 /* produces a machine-processable list of items from the command line words (which may each be more than one word) */
